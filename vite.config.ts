0036b527cab4d6bef43c378c57929b24e0e82cc5
serusko <hruzmatus@gmail.com>
2024-11-02 01:30:31 +0100

    trying to setup multi-entry point lib

--- vite.config.ts
+++ vite.config.ts
@@ -12,39 +12,40 @@ export default defineConfig({
   plugins: [
     react(),
     libInjectCss(),
-    dts({ tsconfigPath: "./tsconfig.app.json" }),
+    dts({ tsconfigPath: "./tsconfig.app.json", exclude: ["**/*.stories.tsx"] }),
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: [
+        resolve(__dirname, "src/index.ts"),
+        resolve(__dirname, "src/Button/index.ts"),
+        resolve(__dirname, "src/Title/index.ts"),
+      ],
       formats: ["es"],
     },
     rollupOptions: {
       input: Object.fromEntries(
-        globSync(["src/components/**/index.tsx", "src/index.ts"]).map(
-          (file) => {
-            // This remove `src/` as well as the file extension from each
-            // file, so e.g. src/nested/foo.js becomes nested/foo
-            const entryName = relative(
-              "src",
-              file.slice(0, file.length - extname(file).length)
-            );
-            // This expands the relative paths to absolute paths, so e.g.
-            // src/nested/foo becomes /project/src/nested/foo.js
-            const entryUrl = fileURLToPath(new URL(file, import.meta.url));
-            return [entryName, entryUrl];
-          }
-        )
+        globSync(["src/**/index.tsx", "src/index.ts"]).map((file) => {
+          const entryName = relative(
+            "src",
+            file.slice(0, file.length - extname(file).length)
+          );
+          const entryUrl = fileURLToPath(new URL(file, import.meta.url));
+          return [entryName, entryUrl];
+        })
       ),
       external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
-        entryFileNames: "[name].js",
+        dir: "dist", // Output directory
+        entryFileNames: (chunk) => `${chunk.name}.js`,
         assetFileNames: "assets/[name][extname]",
         globals: {
           react: "React",
-          "react-dom": "React-dom",
+          "react-dom": "ReactDOM",
           "react/jsx-runtime": "react/jsx-runtime",
         },
+        preserveModules: true, // This option keeps the module structure intact
+        preserveModulesRoot: "src", // Ensures the src/ directory is removed from the output path
       },
     },
   },
